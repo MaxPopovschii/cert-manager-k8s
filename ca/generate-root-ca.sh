f5dce9ae087fa518ee05ff38399e5b56f19a73f1
MaxPopovschii <122737792+MaxPopovschii@users.noreply.github.com>
2025-07-24 08:46:04 +0200

    Update generate-root-ca.sh

--- ca/generate-root-ca.sh
+++ ca/generate-root-ca.sh
@@ -2,6 +2,5 @@
 # ca/generate-root-ca.sh
 
 mkdir -p ca
-openssl genrsa -out ca/root-ca.key 4096
-openssl req -x509 -new -nodes -key ca/root-ca.key -sha256 -days 18250 \
-    -out ca/root-ca.crt -subj "/CN=Kubernetes Root CA"
+openssl req -x509 -new -key ca.key -out ca.crt -days 18250 -subj "/CN=kubernetes-ca"
+
